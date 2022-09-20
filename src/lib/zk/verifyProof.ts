de6e89fc06e21bfec3b45f1601a5853270e910d2
Matej Sima <matej.sima@gmail.com>
2022-09-20 10:23:09 +0200

    add compile before verify

--- src/lib/zk/verifyProof.ts
+++ src/lib/zk/verifyProof.ts
@@ -1,18 +1,40 @@
-import { isReady, verify } from 'snarkyjs';
+import { isReady, verify, PrivateKey, Poseidon, UInt64 } from 'snarkyjs';
 import { RPCCommandMessage, RPCResponseMessage } from '../workers';
 import log from 'loglevel';
 import { JsonProof } from './getProofFromGuess';
+import { challangeHashToContract } from 'zk-p2p-guess/src/helpers';
 
 log.setDefaultLevel('DEBUG');
+
+const privateKeyJSON = {
+  s: '2221840510018593325346886852244642890678926503995097692136933338343303739537',
+};
 
 export const verifyProof = async (
   proof: JsonProof,
   verificationKey: string
 ): Promise<boolean> => {
-  console.log('verifyProof', { proof, verificationKey });
+  console.log('verifyProof', JSON.stringify({ proof, verificationKey }));
   await isReady;
   console.log('is ready');
-  return await verify(proof, verificationKey);
+  const randomPrivateKey = PrivateKey.fromJSON(privateKeyJSON)!;
+
+  /**
+   * Compile the contract before verifying, since there is a bug in snarkyjs that
+   * makes 'verification' unreachable for some unknown reason if any compilation is not run beforehand
+   */
+
+  await challangeHashToContract(
+    randomPrivateKey,
+    Poseidon.hash(new UInt64('0').toFields()).toString()
+  );
+
+  return new Promise((resolve, reject) => {
+    setTimeout(async () => {
+      const valid = await verify(proof, verificationKey);
+      resolve(valid);
+    }, 4000);
+  });
 };
 
 onmessage = async (
